578449ef97d3c10a57882d541fd69c3fdb342d00
Piyush Kumar Singh <21bcs1638@cuchd.in>
2024-03-06 11:02:28 +0530

    added &apos;

--- src/components/AboutBitcoin.tsx
+++ src/components/AboutBitcoin.tsx
@@ -7,7 +7,8 @@ import GradientBox from "./GradientBox";
 
 const AboutBitcoin = () => {
   const content =
-    "For some writers, it isn't getting the original words on paper that's the challenge, but rewriting the first and second drafts. Using the random paragraph generator can be a good way to get into a rewriting routine before beginning the project. In this case, you take the random paragraph and rewrite it so it retains the same meaning, but does so in a better and more concise way. Beginning the day doing this with a random paragraph can make the rewriting of an article, short story, or chapter of a book much easier than trying to begin directly with it.";
+  "For some writers, it isn&apos;t getting the original words on paper that&apos;s the challenge, but rewriting the first and second drafts. Using the random paragraph generator can be a good way to get into a rewriting routine before beginning the project. In this case, you take the random paragraph and rewrite it so it retains the same meaning, but does so in a better and more concise way. Beginning the day doing this with a random paragraph can make the rewriting of an article, short story, or chapter of a book much easier than trying to begin directly with it.";
+
 
   return (
     <div className="flex flex-col gap-3 bg-white rounded-md px-5 py-6 my-3">
